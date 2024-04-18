eb14f8a5a793fa8e19731e57a687912c126e7584
Nicolopez93 <nicolopezarq@gmail.com>
2024-04-17 21:12:04 -0300

    main home

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   
@@ -7,6 +9,9 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </>
   )
 }
