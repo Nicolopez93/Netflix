b46bd7b077420cfc65f638a7d68b8be30f45c512
Nicolopez93 <nicolopezarq@gmail.com>
2024-04-22 20:07:58 -0300

    Sign in y Sign up

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import { Routes, Route } from 'react-router-dom';
 import { AuthContextProvider } from './context/AuthContext';
+import Login from './pages/Login';
+import Singup from './pages/Singup';
+import Account from './pages/Account';
 
 
 function App() {
@@ -12,6 +15,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/singup' element={<Singup />} />
+        <Route path='/account' element={<Account />} />
       </Routes>
     </AuthContextProvider>
     </>
--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='flex justify-between items-center p-4 z-[100] absolute w-full'>
+      <Link to='/'>
       <h1 className='text-red-600 text-4xl font-bold cursor-pointer '>NETFLIX</h1>
+      </Link>
         <div>
+            <Link to='/login'>
             <button className='text-white pr-4'>Sign In</button>
+            </Link>
+            <Link to='/singup'>
             <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Sign Up</button>
+            </Link>
         </div>
     </div>
   )
